1981c94d876469932332bba80052fbe06e256eb4
Adnaan Zubair Shah <136037246+adnaanzshah@users.noreply.github.com>
2024-09-19 21:22:22 +0530

    created first kotlin project

---
+++ basics/basic.kt
@@ -0,0 +1,17 @@
+fun main(){
+    // println("My first kotlin program")
+
+    // val myname="Adnaan Shah"
+    // println(myname)
+
+    // val age=18
+    // val year=2024
+    // val birthyear:Int = (year-age)
+    // println(birthyear)
+
+
+    println("This is a simple program to calculate users age .....")
+    val birthyear:Int = 2005
+    val currentyear:Int=2024
+    println("You are ${currentyear - birthyear} years old.")
+}
